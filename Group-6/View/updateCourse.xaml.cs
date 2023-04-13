a5ed48d7cb2b9b85f4f769ee9b62eb01d06554b9
Lavneet Kaur <kaurlavneet13@gmail.com>
2023-04-12 21:26:01 -0400

    Edit student and Course

---
+++ Group-6/View/updateCourse.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Group_6.View
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string id = sid.Text;
+            string address = txtAddress.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+            
+
+            //save info in database
+
+            NavigationService.GoBack();
+        }
+        private void Button_Cancel_Click (object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
